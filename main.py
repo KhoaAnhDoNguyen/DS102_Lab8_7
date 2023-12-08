552cff63304f0d76c04db960d531390845f46a75
KhoaAnhDoNguyen <117917211+KhoaAnhDoNguyen@users.noreply.github.com>
2023-12-08 10:47:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from PIL import Image
 import pickle as pkl
 import numpy as np
 
-st.title('USA College admission rate prediction')
+st.title('USA college admission rate prediction')
+
+image = Image.open('college admission.jpg')
+st.image(image)
 
 input = open('lr_admit.pkl', 'rb')
 model = pkl.load(input)
@@ -15,7 +18,12 @@ uni_rate = st.number_input('Insert University Rating')
 sop = st.number_input('Insert SOP')
 lor = st.number_input('Insert LOR')
 cgpa = st.number_input('Insert CGPA')
-research = st.radio('Choose Research', [0,1], index=None)
+research = st.radio('Choose Research', [0, 1], index=None)
 
-if gre is not None and toefl is not None and uni_rate is not None and sop is not None and lor is not None and cgpa is not None:
-  pass
+if gre is not None and toefl is not None and uni_rate is not None and sop is not None and lor is not None and cgpa is not None and research is not None:
+    if st.button('Predict'):
+        feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1,-1)
+        result = str((model.predict(feature_vector)[0])[0])
+
+        st.header('Result')
+        st.text(result)
